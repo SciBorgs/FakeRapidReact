83c546f2e0ca37487d3d95a7b4af825ddd1ce829
ethan jones <jonese>
2024-01-12 16:34:03 -0500

    added driveio method

--- src/main/java/org/sciborgs1155/robot/drive/DriveIO.java
+++ src/main/java/org/sciborgs1155/robot/drive/DriveIO.java
@@ -4,4 +4,5 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 public interface DriveIO {
     Command driveDistance(double distance,double speed);
+    Command setSpeeds(double lspeed, double rspeed);
 }
--- src/main/java/org/sciborgs1155/robot/drive/SimDrive.java
+++ src/main/java/org/sciborgs1155/robot/drive/SimDrive.java
@@ -1,15 +1,17 @@
 package org.sciborgs1155.robot.drive;
 
 import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.simulation.ADXRS450_GyroSim;
 import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
 import edu.wpi.first.wpilibj.simulation.EncoderSim;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class SimDrive implements DriveIO { 
     private final DifferentialDrivetrainSim simDriver;
-    private final EncoderSim leftEncoderSim;
-    private final EncoderSim rightEncoderSim;
-    private final ADXRS450_GryoSim gyroSim;
+    private final Field2d field  = new Field2d();
+    private final Pose2d pose = new Pose2d();
     
     public SimDrive() {
         simDriver = new DifferentialDrivetrainSim(
@@ -18,10 +20,7 @@ public class SimDrive implements DriveIO {
             DriveConstants.kDriveGearing,
             DriveConstants.kTrackwidthMeters,
             DriveConstants.kWheelDiameterMeters / 2.0,
-            VecBuilder.fill(0, 0, 0.0001, 0.1, 0.1, 0.005, 0.005));\
-        
-            leftEncoderSim = new EncoderSim(m_leftEncoder);
-            rightEncoderSim = new EncoderSim(m_rightEncoder);
-            gyroSim = new ADXRS450_GyroSim(m_gyro);
+            VecBuilder.fill(0, 0, 0.0001, 0.1, 0.1, 0.005, 0.005));
     }
+
 }
