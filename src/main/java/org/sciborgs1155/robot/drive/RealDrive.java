d610e24d823b3034cf6de804a8066f52bc0cc7f7
HenryLi-0 <bxsci8266@gmail.com>
2024-01-12 17:04:38 -0500

    real drive driveio stuff added

--- src/main/java/org/sciborgs1155/robot/drive/RealDrive.java
+++ src/main/java/org/sciborgs1155/robot/drive/RealDrive.java
@@ -6,10 +6,8 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class RealDrive extends SubsystemBase implements DriveIO {
+public class RealDrive implements DriveIO {
   //  left and right side motors of drive
   private final CANSparkMax leftLeader = new CANSparkMax(DriveConstants.kLeftMotor1Port,MotorType.kBrushed);
   private final CANSparkMax leftFollower = new CANSparkMax(DriveConstants.kLeftMotor2Port,MotorType.kBrushed);
@@ -35,21 +33,15 @@ public class RealDrive extends SubsystemBase implements DriveIO {
     rightFollower.follow(rightLeader, true);
   }
 
-  public Command driveDistance(double distance, double speed){
-    double leftstart=leftEncoder.getPosition();
-    double rightstart=rightEncoder.getPosition();
-    return run(()->diffDrive.arcadeDrive(speed, 0))
-    .until(()-> Math.max(leftEncoder.getPosition()-leftstart, rightEncoder.getPosition()-rightstart) >= distance)
-    .finallyDo(()-> diffDrive.stopMotor());
-    //This code has no PID controller, controlled only with distance and speed. 
-    //That means that if this robot was a fast moving car, it will drive onto the crosswalk and hit an unsuspecting pedistrian. 
-  }
+  @Override public void setLVoltage(double voltage) {leftLeader.setVoltage(voltage);}
+  @Override public void setRVoltage(double voltage) {rightLeader.setVoltage(voltage);}
+  @Override public double getLVoltage() {return leftLeader.getBusVoltage();}
+  @Override public double getRVoltage() {return rightLeader.getBusVoltage();}
+  @Override public double getLDistanceTraveled() {return leftEncoder.getPosition();}
+  @Override public double getRDistanceTraveled() {return rightEncoder.getPosition();}
 
-  @Override
-  public Command setSpeeds(double lspeed, double rspeed){
-    return run(()->diffDrive.tankDrive(lspeed, rspeed));
-  }
 }
 
 
 //First, inspires by https://github.com/wpilibsuite/allwpilib/blob/main/wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/rapidreactcommandbot/subsystems/Drive.java
+//a bit less now...
