73227b817ff4deb79e05aeedcdeed1c5a0a14d7e
ethan jones <jonese>
2024-01-12 16:21:16 -0500

    merging

--- src/main/java/org/sciborgs1155/robot/drive/DriveIO.java
+++ src/main/java/org/sciborgs1155/robot/drive/DriveIO.java
@@ -3,5 +3,5 @@ package org.sciborgs1155.robot.drive;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public interface DriveIO {
-    Command driveDistance(double distance, double speed);
+    
 }
--- src/main/java/org/sciborgs1155/robot/drive/SimDrive.java
+++ src/main/java/org/sciborgs1155/robot/drive/SimDrive.java
@@ -1,7 +1,27 @@
 package org.sciborgs1155.robot.drive;
 
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
+import edu.wpi.first.wpilibj.simulation.EncoderSim;
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class SimDrive implements DriveIO {
-  
+public class SimDrive implements DriveIO { 
+    private final DifferentialDrivetrainSim simDriver;
+    private final EncoderSim leftEncoderSim;
+    private final EncoderSim rightEncoderSim;
+    private final ADXRS450_GryoSim gyroSim;
+    
+    public SimDrive() {
+        simDriver = new DifferentialDrivetrainSim(
+            DriveConstants.kDrivetrainPlant,
+            DriveConstants.kDriveGearbox,
+            DriveConstants.kDriveGearing,
+            DriveConstants.kTrackwidthMeters,
+            DriveConstants.kWheelDiameterMeters / 2.0,
+            VecBuilder.fill(0, 0, 0.0001, 0.1, 0.1, 0.005, 0.005));\
+        
+            leftEncoderSim = new EncoderSim(m_leftEncoder);
+            rightEncoderSim = new EncoderSim(m_rightEncoder);
+            gyroSim = new ADXRS450_GyroSim(m_gyro);
+    }
 }
