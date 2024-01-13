6d2e7fcaa1f6c7fcc1c50607c936afb00c19675d
HenryLi-0 <bxsci8266@gmail.com>
2024-01-13 10:10:32 -0500

    vague name

--- src/main/java/org/sciborgs1155/robot/drive/Drive.java
+++ src/main/java/org/sciborgs1155/robot/drive/Drive.java
@@ -8,15 +8,20 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import monologue.Logged;
+import monologue.Monologue.LogBoth;
 
 public class Drive extends SubsystemBase implements Logged {
   private final PIDController lpid = new PIDController(DriveConstants.kp, DriveConstants.ki, DriveConstants.kd);
   private final PIDController rpid = new PIDController(DriveConstants.kp, DriveConstants.ki, DriveConstants.kd);
   private final SimpleMotorFeedforward lfeedforward = new SimpleMotorFeedforward(DriveConstants.kSVolts, DriveConstants.kVVoltSecondsPerRotation);
   private final SimpleMotorFeedforward rfeedforward = new SimpleMotorFeedforward(DriveConstants.kSVolts, DriveConstants.kVVoltSecondsPerRotation);
+
+  @LogBoth
+  private final Field2d field = new Field2d();
 
   private final DriveIO drive;
   public Drive(DriveIO driverIO) {
@@ -36,6 +41,9 @@ public class Drive extends SubsystemBase implements Logged {
     }
 
     @Override
-    public void periodic() {drive.update();}
+    public void periodic() {
+      drive.update(); 
+      field.setRobotPose(drive.getPose());
+    }
 
   }
